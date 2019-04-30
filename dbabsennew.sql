5eb16c1110725d8f8d72bcefbc9894598d8badb0
daratursina <32977759+daratursina@users.noreply.github.com>
2019-04-30 11:57:37 +0700

    dbnew
    new db

---
+++ dbabsennew.sql
@@ -0,0 +1,183 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 30, 2019 at 06:54 AM
+-- Server version: 10.1.36-MariaDB
+-- PHP Version: 7.2.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dbabsen`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jadwal_kuliah`
+--
+
+CREATE TABLE `jadwal_kuliah` (
+  `id_jadwal` varchar(13) NOT NULL,
+  `id_pertemuan` int(10) NOT NULL,
+  `fk_kode_mk` varchar(10) NOT NULL,
+  `hari` varchar(20) NOT NULL,
+  `jam_masuk` time NOT NULL,
+  `jam_pulang` time NOT NULL,
+  `ruang` varchar(191) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `jadwal_kuliah`
+--
+
+INSERT INTO `jadwal_kuliah` (`id_jadwal`, `id_pertemuan`, `fk_kode_mk`, `hari`, `jam_masuk`, `jam_pulang`, `ruang`) VALUES
+('001', 0, 'IF184605', 'senin', '00:09:55', '00:12:35', '107a');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `log_absen`
+--
+
+CREATE TABLE `log_absen` (
+  `id_absen` int(13) NOT NULL,
+  `id_jadwal` varchar(13) NOT NULL,
+  `nrp_mahasiswa` varchar(191) NOT NULL,
+  `jam_masuk` time NOT NULL,
+  `jam_keluar` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `log_absen`
+--
+
+INSERT INTO `log_absen` (`id_absen`, `id_jadwal`, `nrp_mahasiswa`, `jam_masuk`, `jam_keluar`) VALUES
+(1, '001', '05111540000002', '09:55:00', '12:35:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mahasiswa`
+--
+
+CREATE TABLE `mahasiswa` (
+  `nrp` varchar(10) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `password` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`nrp`, `nama`, `password`) VALUES
+('5115100002', 'Syavira Tiara Z', '123'),
+('5115100005', 'Afrian Mutfiatul Chusnah', '123'),
+('5115100012', 'Aulia Teaku NR', '123'),
+('5115100032', 'Nafingatun Ngaliah', '123'),
+('5115100703', 'Dara Tursina', '123'),
+('5115100707', 'Dara Tursina', '123');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mata_kuliah`
+--
+
+CREATE TABLE `mata_kuliah` (
+  `kode_mk` varchar(10) NOT NULL,
+  `id_pertemuan` int(10) NOT NULL,
+  `mata_kuliah` varchar(191) DEFAULT NULL,
+  `kelas` varchar(10) NOT NULL,
+  `nama_dosen` varchar(191) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `mata_kuliah`
+--
+
+INSERT INTO `mata_kuliah` (`kode_mk`, `id_pertemuan`, `mata_kuliah`, `kelas`, `nama_dosen`) VALUES
+('IF184605', 0, 'Pemrograman Berbasis Kerangka Kerja', 'G', 'Ridho Rahman Hariadi, S.Kom., M.Sc.	'),
+('IF184606', 0, 'Pemrograman Berbasis Kerangka Kerja', 'H', 'Ridho Rahman Hariadi, S.Kom., M.Sc.	');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pertemuan`
+--
+
+CREATE TABLE `pertemuan` (
+  `id_pertemuan` int(10) NOT NULL,
+  `id_kelas` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `jadwal_kuliah`
+--
+ALTER TABLE `jadwal_kuliah`
+  ADD PRIMARY KEY (`id_jadwal`),
+  ADD KEY `fk_kode_mk` (`fk_kode_mk`);
+
+--
+-- Indexes for table `log_absen`
+--
+ALTER TABLE `log_absen`
+  ADD PRIMARY KEY (`id_absen`);
+
+--
+-- Indexes for table `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`nrp`);
+
+--
+-- Indexes for table `mata_kuliah`
+--
+ALTER TABLE `mata_kuliah`
+  ADD PRIMARY KEY (`kode_mk`),
+  ADD UNIQUE KEY `kode_mk` (`kode_mk`),
+  ADD UNIQUE KEY `kode_mk_2` (`kode_mk`);
+
+--
+-- Indexes for table `pertemuan`
+--
+ALTER TABLE `pertemuan`
+  ADD PRIMARY KEY (`id_pertemuan`),
+  ADD UNIQUE KEY `id_pertemuan` (`id_pertemuan`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `jadwal_kuliah`
+--
+ALTER TABLE `jadwal_kuliah`
+  ADD CONSTRAINT `fk_kode_mk` FOREIGN KEY (`fk_kode_mk`) REFERENCES `mata_kuliah` (`kode_mk`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `pertemuan`
+--
+ALTER TABLE `pertemuan`
+  ADD CONSTRAINT `idx_pertemuan` FOREIGN KEY (`id_pertemuan`) REFERENCES `pertemuan` (`id_pertemuan`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
